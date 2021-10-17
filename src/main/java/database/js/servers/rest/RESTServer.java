3b685501aecacef7b4f8c58216eeb897d458375a
Alex Høffner <alex@hoeffner.net>
2021-10-17 15:02:04 +0200

    Update RESTServer.java

--- src/main/java/database/js/servers/rest/RESTServer.java
+++ src/main/java/database/js/servers/rest/RESTServer.java
@@ -153,8 +153,15 @@ public class RESTServer implements RESTConnection
     try
     {
       channel.configureBlocking(false);
-
       channel.connect(port);
+
+      for (int i = 0; i < 8; i++)
+      {
+        Thread.sleep(25);
+        channel.socket().getOutputStream().flush();
+        //Make absolute sure response is flushed
+      }
+      
       channel.configureBlocking(true);
 
       HTTPRequest request = new HTTPRequest("localhost","/connect");      
