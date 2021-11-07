f7da3a4ac415df4981623061b7000a365c7b8ed3
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2021-11-07 15:27:54 +0100

    Update Beautifier.java

--- src/main/java/code/Beautifier.java
+++ src/main/java/code/Beautifier.java
@@ -1,3 +1,14 @@
+/*
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
 package code;
 
 import java.io.File;
@@ -58,7 +69,7 @@ public class Beautifier
     
     if (!line.trim().startsWith("/*"))
     {
-      System.out.println("No open-source header");
+      System.out.println("No open-source header "+file);
       return(null);
     }
     
