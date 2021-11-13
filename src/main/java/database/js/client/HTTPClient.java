02f0785484f146ad831b6ea599bc2b504a8c7b07
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2021-11-13 01:24:18 +0100

    Update HTTPClient.java

--- src/main/java/database/js/client/HTTPClient.java
+++ src/main/java/database/js/client/HTTPClient.java
@@ -94,10 +94,7 @@ public class HTTPClient
       ((SSLSocket) socket).startHandshake();
     }
 
-    this.psize = this.socket.getSendBufferSize();
-
     this.socket.setSoTimeout(15000);
-    this.socket.setSendBufferSize(psize);
     this.socket.getOutputStream().flush();
   }
 }
