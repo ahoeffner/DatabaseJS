180c6c1bf21b30e5b6a389fe594599c16a7d7495
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2023-01-11 12:50:53 +0100

    .

--- src/main/java/code/LicensHeader.java
+++ src/main/java/code/LicensHeader.java
@@ -1,20 +1,20 @@
 /*
-Copyright © 2023 Alex Høffner
+  Copyright © 2023 Alex Høffner
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-and associated documentation files (the “Software”), to deal in the Software without
-restriction, including without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  and associated documentation files (the “Software”), to deal in the Software without
+  restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all copies or
+  substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 package code;
@@ -28,19 +28,19 @@ public class LicensHeader
   private String file = null;
 
   private static final String header =
-  "Copyright © 2023 Alex Høffner\n\n" +
-  "Permission is hereby granted, free of charge, to any person obtaining a copy of this software\n" +
-  "and associated documentation files (the “Software”), to deal in the Software without\n"+
-  "restriction, including without limitation the rights to use, copy, modify, merge, publish,\n" +
-  "distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the\n" +
-  "Software is furnished to do so, subject to the following conditions:\n\n" +
-  "The above copyright notice and this permission notice shall be included in all copies or\n" +
-  "substantial portions of the Software.\n\n" +
-  "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\n" +
-  "BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n" +
-  "NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,\n" +
-  "DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n" +
-  "FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+  "  Copyright © 2023 Alex Høffner\n\n" +
+  "  Permission is hereby granted, free of charge, to any person obtaining a copy of this software\n" +
+  "  and associated documentation files (the “Software”), to deal in the Software without\n"+
+  "  restriction, including without limitation the rights to use, copy, modify, merge, publish,\n" +
+  "  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the\n" +
+  "  Software is furnished to do so, subject to the following conditions:\n\n" +
+  "  The above copyright notice and this permission notice shall be included in all copies or\n" +
+  "  substantial portions of the Software.\n\n" +
+  "  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\n" +
+  "  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n" +
+  "  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,\n" +
+  "  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n" +
+  "  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
 
 
   @SuppressWarnings("unused")
