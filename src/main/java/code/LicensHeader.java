0a15af4fe3fe137a97e12e6a8ab4d12ba2c77735
ahoeffner <87124883+ahoeffner@users.noreply.github.com>
2023-01-11 12:48:16 +0100

    Update LicensHeader.java

--- src/main/java/code/LicensHeader.java
+++ src/main/java/code/LicensHeader.java
@@ -26,16 +26,16 @@ import java.io.FileOutputStream;
 public class LicensHeader
 {
   private String file = null;
-  
+
   private static final String header =
-  "Copyright © 2023 Alex Høffner\n\n" + 
+  "Copyright © 2023 Alex Høffner\n\n" +
   "Permission is hereby granted, free of charge, to any person obtaining a copy of this software\n" +
   "and associated documentation files (the “Software”), to deal in the Software without\n"+
   "restriction, including without limitation the rights to use, copy, modify, merge, publish,\n" +
   "distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the\n" +
-  "Software is furnished to do so, subject to the following conditions:\n\n" + 
+  "Software is furnished to do so, subject to the following conditions:\n\n" +
   "The above copyright notice and this permission notice shall be included in all copies or\n" +
-  "substantial portions of the Software.\n\n" + 
+  "substantial portions of the Software.\n\n" +
   "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\n" +
   "BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n" +
   "NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,\n" +
@@ -73,44 +73,41 @@ public class LicensHeader
     if (file.endsWith(".ts") || file.endsWith("java"))
       this.file = file;
   }
-  
-  
+
+
   public String process() throws Exception
   {
     if (file == null)
       return(null);
-    
-    if (!file.endsWith("LicensHeader.java"))
-      return(null);
 
     return(this.replace());
   }
-  
-  
+
+
   public String replace() throws Exception
   {
     File file = new File(this.file);
     byte[] buf = new byte[(int) file.length()];
 
     FileInputStream in = new FileInputStream(file);
-    
+
     if (in.read(buf) != buf.length)
     {
       System.err.println("Could not read "+this.file);
       return(null);
     }
-    
+
     String code = new String(buf).trim();
-    
+
     if (!code.startsWith("/*"))
     {
       System.err.println("No header in "+this.file);
-      return(null);      
+      return(null);
     }
-    
+
     int start = code.indexOf("*/") + 2;
     code = "/*\n"+LicensHeader.header + "\n*/" + code.substring(start);
-    
+
     return(code);
   }
 
