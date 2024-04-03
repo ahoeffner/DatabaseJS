e166ec42a9fd4be62947e3218e506ec9c025d107
ahoeffner <alex@hoeffner.net>
2024-04-03 14:15:20 +0200

    Update Query.java

--- src/main/java/database/json/handlers/json/parser/Query.java
+++ src/main/java/database/json/handlers/json/parser/Query.java
@@ -113,11 +113,14 @@ public class Query implements SQLObject
             columns[i] = jarr.getString(i);
       }
 
-      if (definition.has(Parser.FILTERS) || source.selflt)
+      boolean filter = source.selflt;
+      if (lock && source.updflt && source.delflt) filter = true;
+
+      if (definition.has(Parser.FILTERS) || filter)
       {
          whcl = new WhereClause(source); whcl.parse(definition);
          bindvalues.addAll(Arrays.asList(whcl.getBindValues()));
-         if (!describe && source.selflt) whcl.filter(source.filter);
+         if (!describe && filter) whcl.filter(source.filter);
       }
 
       this.whcl = whcl;
